86bfb488df7703a9ae5dbeef861207e68538be7f
Mark Toh <marktoh96@gmail.com>
2024-02-02 15:16:07 +0800

    update image base url

--- src/components/OpenDotaImage.jsx
+++ src/components/OpenDotaImage.jsx
@@ -7,7 +7,7 @@ import './OpenDotaImage.css';
  * @returns 
  */
 function OpenDotaImage({ path }) {
-    const BASE_URL = 'https://api.opendota.com'
+    const BASE_URL = "https://cdn.cloudflare.steamstatic.com";
     const completePath = BASE_URL + path
     return (
         <img className="open-dota-image" src={completePath} alt={path} />
